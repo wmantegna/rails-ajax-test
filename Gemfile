d3da1450939fd99f860ee786d27ec71c4b3c0ecd
William Mantegna <wmantegna@gmail.com>
2016-12-30 13:33:25 -0500

    added categories to show another way to use ajax calls with rails

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'responders', '~> 2.0'
 
 # added
 gem 'simple_form'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215230301) do
+ActiveRecord::Schema.define(version: 20161230031425) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ponies", force: :cascade do |t|
     t.string   "name"
